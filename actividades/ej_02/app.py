717c3bca1084bb02b02b204a2db6fee21a1b02bb
Juanse-Lucentini <juanse.lucentini@gmail.com>
2024-09-06 13:29:43 -0300

    feat: agrego scripts.js y cambios al index, boot y app

--- actividades/ej_02/app.py
+++ actividades/ej_02/app.py
@@ -1,1 +1,17 @@
 # Aplicacion del servidor
+from microdot import Microdot, send_file
+
+app = Microdot()
+
+@app.route('/')
+async def index(request):
+    return send_file('index.html')
+
+@app.route('/<dir>/<file>')
+async def index(request, dir, file):
+    return send_file("/{}/{}". format(dir, file))
+
+@app.route('/led/toggle/<led>')
+async def led_toggle(request, led):
+
+app.run(port=80)
--- actividades/ej_02/boot.py
+++ actividades/ej_02/boot.py
@@ -1,1 +1,19 @@
 # Configuracion inicial
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('conectando')
+        sta_if.active(True)
+        sta_if.connect('Cooperadora Alumnos', '')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+from machine import Pin
+
+led1 = Pin(32, Pin.OUT)
+led2 = Pin(33, Pin.OUT)
+led3 = Pin(25, Pin.OUT)
+
+do_connect()
